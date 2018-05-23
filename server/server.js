9c1fc680ed195b080dc450a2b256671b7fd96b41
eugenmorozov <eugen.letterbox@mail.ru>
2018-05-23 18:30:20 +0300

    did the component

---
+++ server/server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+
+
+const serverConfig = {
+    folderWithStatic: '../',
+    defaultPort: 3000
+};
+
+
+const server = http.createServer((request, response) => {
+
+    if (request.url === '/') {
+        request.url = '/index.html';
+    }
+
+    fs.readFile(path.join(__dirname, serverConfig.folderWithStatic, request.url), (error, data) => {
+        if (error) {
+            response.write('Page that you are looking for doesn\'t exist');
+            return;
+        }
+        response.write(data);
+        response.end();
+    });
+
+});
+
+
+server.listen(process.env.PORT || serverConfig.defaultPort);
